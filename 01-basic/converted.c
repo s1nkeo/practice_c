f18207b56247b80e106bb05c9a531cb42da5585e
Sviatoslav Radchenko <svatikr@gmail.com>
2026-06-02 16:58:30 +0200

    New version, but problem with a result

--- 01-basic/converted.c
+++ 01-basic/converted.c
@@ -1,28 +1,24 @@
 #include <stdio.h>
 
 int print_converted(unsigned n, unsigned base) {
-    int res = 0;
-    int mult = 1;
-    unsigned arr[];
-    while (n >= base) {
+    int i = 0;
+    unsigned arr[32];
+    while (n > base) {
+        arr[i] = n % base;
         n = n / base;
-        
-        for (int i = 0; i <= ; i++) {
-            arr[i] = n % base;
+        i++;
+    }  
+        for (int j = i - 1; j >= 0; j--) {
+            printf("%u", arr[j]);
         }
-        
-        for (int j = sizeof(arr - 1); i >= 0; i--) {
-            res += arr[j] * mult;
-            mult *= 10;
-        }
-        return res;
-    }
+     return 0;
+}
     
 int main() {
-    unsigned int n, base;
+    unsigned n, base;
     
-    scanf("%ud %ud", &n, &base);
-    printf("%d", print_converted(n, base);
+    scanf("%u %u", &n, &base);
+    print_converted(n, base);
            
     return 0;
 }

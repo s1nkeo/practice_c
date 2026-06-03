f88d7a1396788d1c17360ed91e9dd816bc9fb981
Sviatoslav Radchenko <svatikr@gmail.com>
2026-06-03 16:24:16 +0200

    two functions pow_mod and spow_mod

--- 01-basic/spow_mod.c
+++ 01-basic/spow_mod.c
@@ -1,29 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
-unsigned spow_mod(unsigned n, unsigned k, unsigned m) {
-	unsigned mult = n % m;
-	unsigned i = 1;
-
-	while (i < k) {
-		unsigned prod = 1;
-		unsigned j = 0;
-
-		while (j < mult) {
-			prod = (prod * n) % m;
-			j++;
-		}
-
-		mult = prod;
-		i++;
-	}
-	return mult;
+unsigned pow_mod(unsigned n, unsigned long long stepen, unsigned modul) {
+    unsigned mult = n % modul;
+    unsigned prod = 1;
+    while (stepen > 0) {
+        if ((stepen % 2) == 1) {
+            prod = (prod * mult) % modul;
+            stepen = stepen - 1;
+        }
+        mult = (mult * mult) % modul;
+        stepen = stepen / 2;
+    }
+    return prod;
 }
 
-int main() {
-	unsigned n, k, m;
-
-	scanf("%u %u %u", &n, &k, &m);
-	printf("%u\n", spow_mod(n, k, m));
-
-	return 0;
+unsigned spow_mod(unsigned n, unsigned long long s_stepen, unsigned modul) {
+    unsigned idx = 1;
+    unsigned stepen = n;
+    for(; idx < s_stepen; idx++) {
+        n = pow_mod(n, stepen, modul);
+    }
+    return n;
 }
+
+int main(void) {
+    int a, b, c;
+    int res;
+    unsigned long long stepen;
+    res = scanf("%d%d%d", &a, &b, &c);
+    assert(res == 3);
+    
+    printf("%d\n", spow_mod(a, b, c));
+    return 0;
+}
+

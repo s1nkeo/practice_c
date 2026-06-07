b82a0c6ac885a39d9a46e1a9f799a9149fb6cd29
Sviatoslav Radchenko <svatikr@gmail.com>
2026-06-07 12:30:29 +0200

    Fibonacci Nim Algorithm

--- 01-basic/fib_num.c
+++ 01-basic/fib_num.c
@@ -55,3 +55,15 @@ int next_turn(int total, int possible) {
 
 	return 1;
 }
+
+int main() {
+    int n, q, res;
+
+    res = scanf("%d%d", &n, &q);
+    assert(res == 2);
+
+    n = next_turn(n, q);
+    printf("%d\n", n);
+
+    return 0;
+}

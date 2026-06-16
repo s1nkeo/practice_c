b1044eedfb90e939d40883baaa84960e18dcebc0
Sviatoslav Radchenko <svatikr@gmail.com>
2026-06-16 14:54:28 +0200

    Add problem_scb.c

---
+++ 03-pointers/problem_scb.c
@@ -0,0 +1,68 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct triple {
+  int x, y, z;
+};
+
+int tcmp(void const *key, void const *elt) {
+  struct triple const *lhs = (struct triple const *)key;
+  struct triple const *rhs = (struct triple const *)elt;
+  return (lhs->y - rhs->y);
+}
+
+typedef int (*cmp_t)(const void *key, const void *elt);
+
+void *cbsearch(const void *key, const void *base, int num, int size, cmp_t cmp) {
+	int l = 0;
+	int r = num - 1;
+	const char *arr = base;
+	int res;
+
+	while (l <= r) {
+		int m = l + (r - l) / 2;
+		const void *a = arr + m * size;
+
+		res = (cmp(key, a));
+		if (res == 0) return (void *)a;
+		if (res < 0) r = m - 1;
+		if (res > 0) l = m + 1;
+	}
+
+	return NULL;
+}
+
+int main() {
+  int i, res, n, last;
+  struct triple found = {-1, -1, -1};
+  struct triple *parr, *answ;
+
+  res = scanf("%d", &n);
+  assert(res == 1);
+  assert(n > 2);
+
+  parr = (struct triple *)calloc(n / 3, sizeof(struct triple));
+
+  for (i = 0; i < n / 3; ++i) {
+    res = scanf("%d %d %d", &parr[i].x, &parr[i].y, &parr[i].z);
+    assert(res == 3);
+    if ((i == n / 7) && ((i % 10) != 0))
+      found = parr[i];
+  }
+
+  answ = (struct triple *)cbsearch(&found, parr, n / 3, sizeof(found), &tcmp);
+
+  if (!answ || (answ == (struct triple *)(parr + n / 3))) {
+    printf("0\n");
+    return 0;
+  }
+
+  printf("%d %d %d\n", answ->x, answ->y, answ->z);
+
+  free(parr);
+  return 0;
+}

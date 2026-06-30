9a6b58f020d21651f408037c120621a3d08f7281
Sviatoslav Radchenko <svatikr@gmail.com>
2026-06-30 15:28:32 +0200

    Add problem_su.c

---
+++ 05-strings/problem_su.c
@@ -0,0 +1,62 @@
+#include <assert.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *strstrci(char const *needle, char const *haystack) {
+	if (!*needle)
+		return (char *)haystack;
+	
+	for (const char *h = haystack; *h != '\0'; h++) {
+		const char *p = h;
+		const char *n = needle;
+
+		while (*p != '\0' && *n != '\0' && tolower((unsigned char)*p) == tolower((unsigned char)*n)) {
+			p++;
+			n++;
+		}
+
+		if (*n == '\0')
+			return (char *)h;
+	}
+	return NULL;
+}
+
+static char *read_word(int *len) {
+  char *text;
+  int res, n, i;
+
+  res = scanf("%d", len);
+  assert(res == 1);
+
+  n = *len;
+  assert(n > 0);
+
+  text = (char *)calloc(n + 1, sizeof(char));
+  assert(text != NULL);
+
+  scanf("%*c");
+
+  for (i = 0; i < n; ++i) {
+    res = scanf("%c", text + i);
+    assert(res == 1);
+  }
+
+  return text;
+}
+
+int main() {
+  int nlen, hlen, res;
+  char *needle, *haystack, *sres;
+
+  haystack = read_word(&hlen);
+  needle = read_word(&nlen);
+
+  sres = strstrci(needle, haystack);
+
+  printf("%s\n", sres);
+
+  free(haystack);
+  free(needle);
+}
